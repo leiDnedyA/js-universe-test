f7b15bef737c23067aaf1f1867c89b269f21f5a2
leiDnedyA <aydendiel@gmail.com>
2024-01-10 17:04:56 -0500

    Testing out htdp/image port

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-import { bigBang, toDraw, onTick, text } from "./js-build/modules/js-wrapper.rkt.js";
+import {
+  bigBang,
+  toDraw,
+  onTick,
+  text,
+  emptyScene,
+  rectangle,
+  frame,
+  placeImagesAlign,
+  posn,
+  posnX,
+  overlayXY,
+  underlayXY,
+  overlay,
+  underlay,
+  aboveAlign,
+  above,
+  besideAlign,
+  beside
+} from "./js-build/modules/js-wrapper.rkt.js";
 
 const INIT_STATE = 100;
 const MAX_TEXT_SIZE = 100;
 const MIN_TEXT_SIZE = 50;
 
 const draw = toDraw((ws) => {
-  return text("Hello, world!", Math.abs(ws), "black");
+  return besideAlign(
+    "above",
+    [
+    text("Hello, world!", Math.abs(ws), "black"),
+    emptyScene(500, 500, "gray")
+    ]
+  )
+  // return overlayXY(
+  //   text("Hello, world!", Math.abs(ws), "black"),
+  //   30,
+  //   30,
+  //   emptyScene(500, 500, "gray"),
+  // );
+  // return overlayAlign("center", "top", [text("Hello, world!", Math.abs(ws), "black"), rectangle(20, 20, "solid", "red"), emptyScene(500, 500, "gray")]);
 });
 
 const tick = onTick((ws) => {
@@ -20,6 +52,6 @@ const tick = onTick((ws) => {
     }
     return ws + 1;
   }
-}, .01);
+}, 0.01);
 
 bigBang(INIT_STATE, [draw, tick]);
